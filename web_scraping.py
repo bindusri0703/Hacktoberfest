4b65ba2a1aa2bb57d8d0b0d1dd66a936ba331269
bindusri0703 <142156206+bindusri0703@users.noreply.github.com>
2023-10-31 12:46:46 +0530

    Create web_scraping.py

---
+++ web_scraping.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Specify the URL of the website you want to scrape
+url = 'https://example.com'
+
+# Send an HTTP GET request to the URL
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the HTML content of the page using Beautiful Soup
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Find and extract specific data from the page
+    # For example, let's extract all the links on the page
+    links = soup.find_all('a')
+    
+    # Print the links
+    for link in links:
+        print(link.get('href'))
+else:
+    print('Failed to retrieve the web page. Status code:', response.status_code)
